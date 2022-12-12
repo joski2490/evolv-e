2cc79aba65b13191083cad3385dec5ddfe3ba7a3
joski2490 <68449917+joski2490@users.noreply.github.com>
2022-12-12 15:29:01 -0600

    Update Alimento.js

--- public/Classes/Alimento.js
+++ public/Classes/Alimento.js
@@ -6,7 +6,7 @@ class Alimento{
         this.posicao = new Vetor(x, y);
         this.raio = raio;
         // a energia do pedaço de alimento é proporcinal à sua área
-        this.energia_alimento = Math.floor(Math.PI * Math.pow(this.raio, 2)) * 15;
+        this.energia_alimento = Math.floor(Math.PI * Math.pow(this.raio, 4)) * 15 // (Size, 2) × 15
 
         Alimento.alimentos.push(this);
 
