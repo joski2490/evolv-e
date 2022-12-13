fac101b9352fe37d0e5405d3b4cee9b2d59d6baf
joski2490 <68449917+joski2490@users.noreply.github.com>
2022-12-12 20:50:37 -0600

    Update Herbivoro.js

--- public/Classes/Herbivoro.js
+++ public/Classes/Herbivoro.js
@@ -191,7 +191,7 @@ class Herbivoro extends Organismo{
         // var direcao = this.vel.headingDegs();
         c.beginPath();
         // desenhaOval(c, this.posicao.x, this.posicao.y, this.raio*2, this.raio, 'red');
-        c.ellipse(this.posicao.x, this.posicao.y, this.raio * 0.7, this.raio * 1.1, this.vel.headingRads() - Math.PI/2, 0, Math.PI * 2);
+        c.ellipse(this.posicao.x, this.posicao.y, this.raio * 0.4, this.raio * 1.9, this.vel.headingRads() - Math.PI/2, 0, Math.PI * 2);//0.7, 1.1
         // console.log(this.vel.headingDegs());
         if(Carnivoro.highlight) {
             c.fillStyle = "rgba(" + this.cor.substr(4).replace(")","") + ",0.15)";
