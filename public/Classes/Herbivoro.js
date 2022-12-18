aea490e0bc6cb8d3916ce812585910ddee2f038e
joski2490 <68449917+joski2490@users.noreply.github.com>
2022-12-18 16:11:14 -0600

    Update Herbivoro.js

--- public/Classes/Herbivoro.js
+++ public/Classes/Herbivoro.js
@@ -191,23 +191,21 @@ class Herbivoro extends Organismo{
         // var direcao = this.vel.headingDegs();
         c.beginPath();
         // desenhaOval(c, this.posicao.x, this.posicao.y, this.raio*2, this.raio, 'red');
-        c.ellipse(this.posicao.x, this.posicao.y, this.raio * 0.7, this.raio * 1.1, this.vel.headingRads() - Math.PI/2, 0, 2 * Math.PI);
-        //0.7, 1.1 // pi/2, 0, pi*2
+        c.ellipse(this.posicao.x, this.posicao.y, this.raio * 0.7, this.raio * 1.1, this.vel.headingRads() - Math.PI/2, 0, Math.PI * 2);
         // console.log(this.vel.headingDegs());
         if(Carnivoro.highlight) {
             c.fillStyle = "rgba(" + this.cor.substr(4).replace(")","") + ",0.15)";
             c.strokeStyle = "rgba(" + this.cor.substr(4).replace(")","") + ",0.15)";
         } else {
             c.fillStyle = this.cor;
-            //c.strokeStyle = this.cor;
+            c.strokeStyle = this.cor;
         }
 
         c.fill();
-        //c.stroke();
         // desenhando o raio de detecção
-          //   c.beginPath();
-            // c.arc(this.posicao.x, this.posicao.y, this.raio_deteccao, 0, Math.PI * 2);
-          //   c.strokeStyle = "grey";
-         //    c.stroke();
+        //     c.beginPath();
+        //     c.arc(this.posicao.x, this.posicao.y, this.raio_deteccao, 0, Math.PI * 2);
+        //     c.strokeStyle = "grey";
+        //     c.stroke();
     }
 }
